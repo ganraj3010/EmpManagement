05159e02c866009adc1aac19ae593ec868981b08
ganraj3010 <ganra3010@gmail.com>
2024-02-12 22:54:07 +0530

    empLogIn

--- EmpManagement/EmpManagement/force-app/main/default/lwc/empLogIn/empLogIn.js
+++ EmpManagement/EmpManagement/force-app/main/default/lwc/empLogIn/empLogIn.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class EmpLogIn extends LightningElement {}
+export default class EmpLogIn extends LightningElement {
+    @track
+    loginDisabled = true;
+    userName
+    password
+    setLogin(event){
+        this.userName = event.detail.value
+        if(this.userName && this.password){
+            this.loginDisabled = false
+        }else{
+            this.loginDisabled = true
+        }
+    }
+    setPassword(event){
+        this.password = event.detail.value
+        if(this.userName && this.password){
+            this.loginDisabled = false
+        }else{
+            this.loginDisabled = true
+        }
+    }
+}
